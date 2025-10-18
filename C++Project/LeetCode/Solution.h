3886be048968306f21d91745b07ad8e03299c3ec
liuyuhao <3284871599@qq.com>
2025-10-18 16:50:53 +0800

    提交忽略文件，添加方法

--- C++Project/LeetCode/Solution.h
+++ C++Project/LeetCode/Solution.h
@@ -1,8 +1,37 @@
 #pragma once
 
+#include <vector>
+#include <unordered_map>
+
+#include "Extension.h"
+
+using namespace std;
 
 class Solution {
 
-	
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		unordered_map<int, int> maps;
+		for (int i = 0;i < nums.size();i++) {
+			int temp = target - nums[i];
+			if (maps.count(temp)) {
+				cout << maps[temp] << "," << i << endl;
+				return { maps[temp],i };
+			}
+			maps[nums[i]] = i;
+		}
+		return {};
+	}
 
+public:
+	ListNode* addTwoSum(ListNode* l1, ListNode* l2) {
+		int carry = 0;
+
+		ListNode* head = nullptr;
+		ListNode* current = nullptr;
+
+		while (l1 != nullptr && l2 != nullptr) {
+			int value = l1->val + l2->val + carry;
+		}
+	}
 };
