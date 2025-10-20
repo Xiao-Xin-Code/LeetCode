3d034cedc6458facf187dfea62f7c158167a5d9c
liuyuhao <3284871599@qq.com>
2025-10-20 12:00:26 +0800

    上传方法

--- C++Project/LeetCode/LeetCode.cpp
+++ C++Project/LeetCode/LeetCode.cpp
@@ -9,7 +9,9 @@ using namespace std;
 int main()
 {
     Solution solution;
-    vector<int> ver = vector<int>{ 1, 2, 3, 4, 5 };
-    vector<int> temp = solution.twoSum(ver, 6);
+   /* vector<int> ver = vector<int>{ 1, 2, 3, 4, 5 };
+    vector<int> temp = solution.twoSum(ver, 6);*/
+
+    cout << solution.myAtoi("-12-3") << endl;
 
 }
--- C++Project/LeetCode/Solution.h
+++ C++Project/LeetCode/Solution.h
@@ -221,4 +221,25 @@ public:
 		}
 	}
 
+public:
+	int myAtoi(string s) {
+		bool isNeg = false;
+		int cur = 0;
+		for (int i = 0;i < s.length();i++) {
+			if (s[i] >= '0' && s[i] <= '9') {
+				cur = cur * 10 + (s[i] - '0');
+			}
+			else {
+				if (i == 0 && s[i] == '-') {
+					isNeg = true;
+				}
+				else {
+					break;
+				}
+			}
+		}
+
+		return isNeg ? -cur : cur;
+	}
+
 };
