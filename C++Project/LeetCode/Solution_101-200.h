145f3ee5b554b5c39b2d50db284b5281c313c62f
liuyuhao <3284871599@qq.com>
2025-12-24 17:45:52 +0800

    add function

--- C++Project/LeetCode/Solution_101-200.h
+++ C++Project/LeetCode/Solution_101-200.h
@@ -1288,7 +1288,34 @@ namespace Solution_101_200 {
 				rightSideViewExtension(tempStack, result);
 			}
 		}
+		
+		int numIslands(vector<vector<char>>& grid) {
+			int count = 0;
+			for (int i = 0;i < grid.size();++i) {
+				for (int j = 0;j < grid[i].size();++j) {
+					if (grid[i][j] == '1') {
+						count++;
+						numIslandsExtension(grid, i, j);
+					}
+				}
+			}
+			return count;
+		}
 
+		void numIslandsExtension(vector<vector<char>>& grid,int i,int j) {
+			if (i >= 0 && i < grid.size() && j >= 0 && j < grid[0].size()) {
+				if (grid[i][j] == '0') return;
+				grid[i][j] = '0';
+				numIslandsExtension(grid, i - 1, j - 1);
+				numIslandsExtension(grid, i - 1, j);
+				numIslandsExtension(grid, i - 1, j + 1);
+				numIslandsExtension(grid, i, j - 1);
+				numIslandsExtension(grid, i, j + 1);
+				numIslandsExtension(grid, i + 1, j - 1);
+				numIslandsExtension(grid, i + 1, j);
+				numIslandsExtension(grid, i + 1, j + 1);
+			}
+		}
 
 		//221
 	public:
